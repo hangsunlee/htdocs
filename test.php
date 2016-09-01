9c7957e1eee10e2c861fe04270417a30348a279a
hangsunlee <hslee22@sen.go.kr>
2016-09-01 10:24:02 +0900

    HTML에 코드에 PHP를 삽입하려면

---
+++ test.php
@@ -0,0 +1,26 @@
+<?php
+	phpinfo();
+?>
+<!doctype html>
+<html lang="en">
+ <head>
+  <meta charset="UTF-8">
+  <meta name="Generator" content="EditPlus®">
+  <meta name="Author" content="">
+  <meta name="Keywords" content="">
+  <meta name="Description" content="">
+  <title>오늘은 첫번째 php수업</title>
+ </head>
+ <body>
+ <ul>
+ 	<li>1번</li>
+ 	<li>2번</li>
+	<?php
+       print "안녕 나는 오늘 기분이 별로 안좋으니깐 조심해 경식아";
+    ?>
+ 	<li>3번</li>
+ 	<li>4번</li>
+ </ul>
+
+ </body>
+</html>
