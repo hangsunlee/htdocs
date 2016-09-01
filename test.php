ba7d3853ebc3ddc4824d9bfa35dbd6ce1be9676f
hangsunlee <hslee22@sen.go.kr>
2016-09-01 10:39:54 +0900

    상수를 적용하려면

--- test.php
+++ test.php
@@ -1,5 +1,6 @@
+
 <?php
-	phpinfo();
+//	phpinfo();
 ?>
 <!doctype html>
 <html lang="en">
@@ -18,7 +19,17 @@
 	<?php
        print "안녕 나는 오늘 기분이 별로 안좋으니깐 조심해 경식아";
     ?>
- 	<li>3번</li>
+ 	<li>3번째 예제<?php
+		print PHP_VERSION;
+		print PHP_OS;
+		define("HELLO","안녕하세요");
+		print HELLO."<br>";
+		define("_18","신발");
+		print _18;
+		print __DIR__;
+		
+	?></li>
+
  	<li>4번</li>
  </ul>
 
